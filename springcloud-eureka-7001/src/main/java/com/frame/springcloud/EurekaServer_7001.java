1074757eee6d82f2c670bbed53df4c68751cf57c
魏陈昊 <525691465@qq.com>
2020-11-21 20:56:54 +0800

    添加eureka注册中心

---
+++ springcloud-eureka-7001/src/main/java/com/frame/springcloud/EurekaServer_7001.java
@@ -0,0 +1,13 @@
+package com.frame.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer  //Eureka服务端的启动类
+public class EurekaServer_7001 {
+    public static void main(String[] args){
+        SpringApplication.run(EurekaServer_7001.class,args);
+    }
+}
