2f6110957e33730953c2b8ca86b5a81bea08e800
魏陈昊 <525691465@qq.com>
2020-11-23 20:09:08 +0800

    添加一个测试负载均衡用的uac提供者,consumer层和zuul的负载均衡测试成功

---
+++ springcloud-provider/springcloud-provider-uac2/src/main/java/com/frame/springcloud/controller/DeptController.java
@@ -0,0 +1,44 @@
+package com.frame.springcloud.controller;
+
+import com.frame.springcloud.pojo.Dept;
+import com.frame.springcloud.service.DeptService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/dept")
+public class DeptController {
+
+    @Autowired
+    private DeptService deptService;
+
+    @GetMapping("/list")
+    public List<Dept> queryAll(){
+        return deptService.queryAll();
+    }
+
+    @GetMapping("/get/{id}")
+    @HystrixCommand(fallbackMethod = "hystrixGet")
+    public Dept get(@PathVariable("id") Long id){
+        Dept dept = deptService.queryById(id);
+        if (dept == null){
+            throw new RuntimeException("id=>"+id+",不存在该用户，或信息无法找到");
+        }
+        return dept;
+    }
+
+    //备选方案
+    public Dept hystrixGet(@PathVariable("id") Long id){
+        return new Dept()
+                .setDeptno(id)
+                .setDname("id=>"+id+"没有对应的信息,null--@hystrix")
+                .setDb_source("no this database in mysql");
+    }
+}
